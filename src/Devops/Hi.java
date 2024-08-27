019a624900087e7d3ac165d32902427779c8e656
AdmiN <AdmiN@ITWS037>
2024-08-27 11:40:57 +0530

    changes username and password

--- src/Devops/Hi.java
+++ src/Devops/Hi.java
@@ -1,11 +1,24 @@
 package Devops;
+import java.util.*;
 
 public class Hi {
-	Scanner sc = new Scanner(System.in);
-	System.out.println("enter username");
-	String ip1 = sc.nextLine();
-	System.out.println("enter password");
-	String ip2 = sc.nextLine();
-	System.out.println("Username "+ip1);
-	System.out.println("Password "+ip2);
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String u_name = "shahidbysui@gmail.com";
+		String pwd = "suiii@2004";
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter username");
+		String ip1 = sc.nextLine();
+		System.out.println("enter password");
+		String ip2 = sc.nextLine();
+		if(u_name.equals(ip1) && pwd.equals(ip2))
+		{
+			System.out.println("Login successfull");
+		}
+		else {
+			System.out.println("Login Failed");
+		}
+	}
+
 }
