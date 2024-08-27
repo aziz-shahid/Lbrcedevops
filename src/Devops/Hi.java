ed464337cc990f6b9837e14fb31cdfe71cdd1466
AdmiN <AdmiN@ITWS037>
2024-08-27 10:14:04 +0530

    adds signup functionality

--- src/Devops/Hi.java
+++ src/Devops/Hi.java
@@ -1,5 +1,12 @@
 package Devops;
 
 public class Hi {
-
+	Scanner sc = new Scanner(System.in);
+	System.out.println("enter username");
+	String ip1 = sc.nextLine();
+	System.out.println("enter password");
+	String ip2 = sc.nextLine();
+	System.out.println("Username "+ip1);
+	System.out.println("updated");
+	System.out.println("Password "+ip2);
 }
